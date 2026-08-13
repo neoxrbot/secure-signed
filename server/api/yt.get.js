19e17c2aad87e77a816ede1751ef6ff4d2b8cab2
neoxr <neoxr@null.net>
2026-08-13 22:24:53 +0000

    feat: yt

--- server/api/yt.get.js
+++ server/api/yt.get.js
@@ -2,17 +2,21 @@ import { getCloudflareEnv } from '../utils/cloudflare.js'
 import { Innertube } from 'youtubei.js/cf-worker'
 
 export default defineEventHandler(async (event) => {
-   const env = getCloudflareEnv(event)
-   const query = getQuery(event)
-   const url = query.url
+   try {
+      const env = getCloudflareEnv(event)
+      const query = getQuery(event)
+      const url = query.url
 
-   if (!url) {
-      return { status: false, msg: 'URL parameter is required' }
+      if (!url) {
+         return { status: false, msg: 'URL parameter is required' }
+      }
+
+      const yt = await Innertube.create();
+
+      const video = await yt.getInfo("jNQXAC9IVRw");
+
+      return video
+   } catch (err) {
+      return { status: false, msg: err.message }
    }
-
-   const yt = await Innertube.create();
-
-   const video = await yt.getInfo("jNQXAC9IVRw");
-
-   return new Response(JSON.stringify(video), { headers: { 'Content-Type': 'application/json' } })
 })
