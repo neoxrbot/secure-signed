782fb997a9530b4b44c28f6d887a5fb72a17a8c5
neoxr <neoxr@null.net>
2026-08-17 13:15:36 +0000

    fix: import markdown css

--- server/api/endpoints.get.js
+++ server/api/endpoints.get.js
@@ -1,10 +1,12 @@
+import { jsonResponse } from '../utils/index.js'
+
 export default defineEventHandler((event) => {
    const config = useRuntimeConfig(event)
    const endpoints = config.endpointsList || []
 
-   return {
+   return jsonResponse(event, {
       status: true,
       total: endpoints.length,
       endpoints: endpoints
-   }
+   })
 })
